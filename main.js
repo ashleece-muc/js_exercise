1ad419f615d73388ea4b48bac3729e6e2b642960
ashleece-muc <asnate.muceniece@gmail.com>
2022-07-28 00:15:38 +0300

    changes7

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 //String, Numbers, Boolean, null, undefined
 
-const name = "John";
-const age = 30;
+//const name = "John";
+//const age = 30;
 
 // Concatention
 //console.log("My name is" + name + "and I am" + age);
 //Template String
 //console.log(`My name is ${name} and I am ${age}`);
 //console.log("hello");
+
+
+
+const s = 'Hello World!'
+console.log(s.substring(0, 5).toLocaleUpperCase());
