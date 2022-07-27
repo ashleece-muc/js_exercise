c7153c6871e3edb830114aed87eed05b156f9d9d
ashleece-muc <asnate.muceniece@gmail.com>
2022-07-28 00:23:05 +0300

    changes

--- main.js
+++ main.js
@@ -11,5 +11,10 @@
 
 
 
-const s = 'Technology, computers, it, code';
-console.log(s.split(','));
+//const s = 'Technology, computers, it, code';
+//console.log(s.split(','));
+
+
+//Arrays- variables that hold multiple values
+const numbers = new Array(1, 2, 3, 4, 5);
+console.log(numbers);
